1a45827d08013212632e1cde3341d13ddf7eedd0
Jason Martinez <jasonmar08@gmail.com>
2022-09-08 16:42:18 -0400

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import NavBar from './components/NavBar'
 import './App.css'
 
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+      <main></main>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+// import { NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+
+  return (
+    <nav>
+      <div className='navbar'>
+        <h1>This is the Navbar</h1>
+        {/* <NavLink to='/' className='nav-link'>Welcome</NavLink>
+        <NavLink to='/Home' className='nav-link'>Home</NavLink>
+        <NavLink to='/Projects' className='nav-link'>Projects</NavLink>
+        <NavLink to='/Contact' className='nav-link'>Contact Me</NavLink> */}
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
