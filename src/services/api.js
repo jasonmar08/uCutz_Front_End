875e66f361665171389ae6453757cc8e4b4ed0a5
Jason Martinez <jasonmar08@gmail.com>
2022-10-29 14:41:36 -0400

    add meta tag and remove https from BASE_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 // export const BASE_URL = 'http://localhost:3001'
-export const BASE_URL = 'https://ec2-44-204-41-235.compute-1.amazonaws.com:3001'
+export const BASE_URL = 'http://ec2-44-204-41-235.compute-1.amazonaws.com:3001'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
