1ea04d52294ab56b48aa67690f646fdbb69d99ee
Jason Martinez <jasonmar08@gmail.com>
2023-01-11 02:32:47 -0500

    change BASE_URL to EC2 instance

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 // export const BASE_URL = 'http://localhost:3001'
-export const BASE_URL = 'ec2-54-164-52-216.compute-1.amazonaws.com'
+export const BASE_URL = 'http://ec2-54-164-52-216.compute-1.amazonaws.com:3001'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
